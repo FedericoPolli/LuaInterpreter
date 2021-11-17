882e6bbdf2bb49f4b5efad7c18372bfc67b8d24f
FedericoPolli <Polli.f@outlook.it>
2021-11-17 12:59:08 +0100

    First attempt

--- src/main/java/testlib/BookLibrary.java
+++ src/main/java/testlib/BookLibrary.java
@@ -1,10 +1,19 @@
 package testlib;
 
 import com.sun.jna.Library;
+import com.sun.jna.Structure;
 
 public interface BookLibrary extends Library {
-    void printBook();
-    void printBooks();
-    double averageBookLen();
-    int totalBooksLen();
+
+    @Structure.FieldOrder({"title", "author", "series", "book_len"})
+    public static class Books extends Structure{
+        public static class ByReference extends Books implements Structure.ByReference {}
+        public String title, author, series;
+        public int book_len;
+    }
+
+    void printBook(Books.ByReference book);
+    void printBooks(Books.ByReference[] books);
+    double averageBookLen(Books.ByReference[] books);
+    int totalBooksLen(Books.ByReference[] books);
 }
--- src/main/java/testlib/Main.java
+++ src/main/java/testlib/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         BookLibrary bookLibrary = Native.load("/media/sf_Federico/Units/Quinto Anno/Tirocinio/Code/C/JNA_C_Library/libBooks.so", BookLibrary.class);
-        bookLibrary.printBook();
+        BookLibrary.Books.ByReference[] books = (BookLibrary.Books.ByReference[]) new BookLibrary.Books.ByReference().toArray(3);
+        books[0].title = "The Fellowship of the Ring";
+        books[0].author = "J. R. R. Tolkien";
+        books[0].series = "The Lord of the Rings";
+        books[0].book_len = 425;
+        bookLibrary.printBooks(books);
     }
 }
